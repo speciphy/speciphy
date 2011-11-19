cba29d9fb5c919bdc9844f320a0392502be6cb6a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-11-20 05:05:14 +0900

    Improve: Implemented describe() and context() DSL.

--- src/Speciphy/DSL.php
+++ src/Speciphy/DSL.php
@@ -1,11 +1,48 @@
 <?php
 namespace Speciphy\DSL;
-use Speciphy\Describe;
 
-function describe($subjectName, $expectations) {
-    return new Describe($subjectName, $expectations);
+use Speciphy\ExampleGroup;
+use Speciphy\Example;
+use Speciphy\Pending;
+
+function describe($description, $specElements) {
+    $exampleGroup = new ExampleGroup($description);
+
+    foreach ($specElements as $key => $value) {
+        if (is_string($key) && $value instanceof \Closure) {
+            $exampleGroup->addChild(new Example($key, $value));
+        } else if (is_int($key)) {
+            if ($value instanceof ExampleGroup) {
+                $exampleGroup->addChild($value);
+            } else if (is_string($value)) {
+                $exampleGroup->addChild(new Pending($value));
+            }
+        } else if (is_string($key)) {
+            switch ($key) {
+            case 'subject':
+                $exampleGroup->setSubjectBlock($value);
+                break;
+            case 'before':
+                $exampleGroup->setBeforeHook($value);
+                break;
+            case 'after':
+                $exampleGroup->setAfterHook($value);
+                break;
+            case 'before_all':
+            case 'beforeAll':
+                $exampleGroup->setBeforeAllHook($value);
+                break;
+            case 'after_all':
+            case 'afterAll':
+                $exampleGroup->setAfterAllHook($value);
+                break;
+            }
+        }
+    }
+
+    return $exampleGroup;
 }
 
-function context($subjectName, $expectations) {
-    return new Describe($subjectName, $expectations);
+function context($description, $examples) {
+    return describe($description, $examples);
 }
