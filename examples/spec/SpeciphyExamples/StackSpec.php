37bd2ee99c6fde1548e0a3c4f513d888e176bd3c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-11-27 18:55:55 +0900

    Fix: Example spec file.

--- examples/spec/SpeciphyExamples/StackSpec.php
+++ examples/spec/SpeciphyExamples/StackSpec.php
@@ -5,22 +5,18 @@ use SpeciphyExamples\Stack;
 
 return describe('Stack', array(
     context('When empty', array(
-        'subject' => function () {
-            return new Stack;
-        },
+        subject(function () { return new Stack; }),
 
-        'It should be empty.' => function ($it) {
+        it('should be empty', function ($it) {
             $it->should()->beEmpty();
-        },
+        }),
     )),
 
     context('When has 1 item', array(
-        'subject' => function () {
-            return new Stack(array(1));
-        },
+        subject(function () { return new Stack(array(1)); }),
 
-        'It should not be empty.' => function ($it) {
+        it('should not be empty', function ($it) {
             $it->shouldNot()->beEmpty();
-        },
+        }),
     )),
 ));
