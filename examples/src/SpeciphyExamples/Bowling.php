71b8476c5147e39258c9eb0325234376d24d42a1
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-13 04:42:30 +0900

    Change: Replaced with Bowling example.

---
+++ examples/src/SpeciphyExamples/Bowling.php
@@ -0,0 +1,16 @@
+<?php
+namespace SpeciphyExamples;
+
+class Bowling
+{
+    protected $_score = 0;
+
+    public function hit($pins)
+    {
+    }
+
+    public function getScore()
+    {
+        return $this->_score;
+    }
+}
