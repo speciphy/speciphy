93bc4ae56614a3f17a2af5cb263c0d93f67c1265
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-13 20:08:51 +0900

    Change: Removed DSL notation with array.

--- src/Speciphy/DSL.php
+++ src/Speciphy/DSL.php
@@ -6,47 +6,19 @@ use Speciphy\Example;
 use Speciphy\Pending;
 use Speciphy\Subject;
 
-function describe($description, $specElements = NULL) {
+function describe($description) {
     $exampleGroup = new ExampleGroup($description);
 
-    if (isset($specElements) && ! is_array($specElements)) {
-        $args = func_get_args();
-        array_shift($args);
-        $specElements = $args;
-    }
-
-    if (isset($specElements)) {
-        foreach ($specElements as $key => $value) {
-            if (is_int($key)) {
-                if ($value instanceof ExampleGroup ||
-                    $value instanceof Example ||
-                    $value instanceof Pending) {
-                    $exampleGroup->addChild($value);
-                } else if ($value instanceof Subject) {
-                    $exampleGroup->setSubject($value);
-                } else if (is_string($value)) {
-                    $exampleGroup->addChild(new Pending($value));
-                }
-            } else if (is_string($key)) {
-                switch ($key) {
-                case 'subject':
-                    $exampleGroup->setSubject(new Subject($value));
-                    break;
-                case 'before':
-                    $exampleGroup->setBeforeHook($value);
-                    break;
-                case 'after':
-                    $exampleGroup->setAfterHook($value);
-                    break;
-                case 'before_all':
-                case 'beforeAll':
-                    $exampleGroup->setBeforeAllHook($value);
-                    break;
-                case 'after_all':
-                case 'afterAll':
-                    $exampleGroup->setAfterAllHook($value);
-                    break;
-                }
+    $args = func_get_args();
+    array_shift($args);
+    foreach ($args as $key => $value) {
+        if (is_int($key)) {
+            if ($value instanceof ExampleGroup ||
+                $value instanceof Example ||
+                $value instanceof Pending) {
+                $exampleGroup->addChild($value);
+            } else if ($value instanceof Subject) {
+                $exampleGroup->setSubject($value);
             }
         }
     }
@@ -54,7 +26,7 @@ function describe($description, $specElements = NULL) {
     return $exampleGroup;
 }
 
-function context($description, $examples = NULL) {
+function context($description) {
     return call_user_func_array('\\Speciphy\\DSL\\describe', func_get_args());
 }
 
