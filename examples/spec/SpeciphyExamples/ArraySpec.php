dc97e0e2e3abb9dedd5d3e3139a2209c4ed777e7
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-13 19:55:14 +0900

    Fix: DSL notation.

--- examples/spec/SpeciphyExamples/ArraySpec.php
+++ examples/spec/SpeciphyExamples/ArraySpec.php
@@ -1,24 +1,24 @@
 <?php
 namespace Speciphy\DSL;
 
-return describe('Array', array(
-    context('When empty', array(
-        'subject' => function () {
+return describe('Array',
+    context('When empty',
+        subject(function () {
             return array();
-        },
+        }),
 
         it('should be empty', function ($it) {
             $it->should->beEmpty();
-        }),
-    )),
+        })
+    ),
 
-    context('When has 1 item', array(
-        'subject' => function () {
+    context('When has 1 item',
+        subject(function () {
             return array(1);
-        },
+        }),
 
         it('should not be empty', function ($it) {
             $it->shouldNot->beEmpty();
-        }),
-    )),
-));
+        })
+    )
+);
