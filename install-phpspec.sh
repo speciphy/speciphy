7363dfbc884749fa35c2a30324e55d79870437de
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-14 02:41:34 +0900

    Fix: Build settings

--- install-phpspec.sh
+++ install-phpspec.sh
@@ -4,5 +4,7 @@ then
   mkdir vendor
 fi
 wget http://pear2.php.net/pyrus.phar -O pyrus.phar
+php pyrus.phar ./vendor di pear.phpspec.net
+php pyrus.phar ./vendor di pear.phpunit.de
 php pyrus.phar ./vendor set bin_dir ./vendor/bin
-php pyrus.phar ./vendor install pear.phpspec.net/PHPSpec-1.2.2beta
+php pyrus.phar ./vendor install phpspec/PHPSpec-1.2.2beta
