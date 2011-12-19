125379c22aa69a62d76e15d67b2df2ff9518d5a4
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-19 21:47:05 +0900

    Fix: Vendoring script.

--- install-vendor.sh
+++ install-vendor.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-if [ ! -d ./vendor ]
-then
+if [ ! -d ./vendor ]; then
   mkdir vendor
 fi
-wget http://pear2.php.net/pyrus.phar -O pyrus.phar
+if [ ! -f pyrus.phar ]; then
+  wget http://pear2.php.net/pyrus.phar
+fi
 php pyrus.phar ./vendor di pear.phpspec.net
 php pyrus.phar ./vendor di pear.phpunit.de
 php pyrus.phar ./vendor set bin_dir ./vendor/bin
