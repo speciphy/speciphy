9f506dd6bd25b2a646b146be07409d85dddac926
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-23 12:38:03 +0900

    Change: Replaced install-vendor.sh with onion.phar (ref:2b6118bf8fab6c1ffc284ace4f77f877501ada36)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/Speciphy/Tests/TestCase.php';
-require_once __DIR__ . '/../vendor/php/PHPSpec/Loader/UniversalClassLoader.php';
+require_once __DIR__ . '/../vendor/pear/PHPSpec/Loader/UniversalClassLoader.php';
 $loader = new \PHPSpec\Loader\UniversalClassLoader();
 $loader->registerNamespace('PHPSpec', __DIR__ . '/../vendor/php');
 $loader->registerNamespace('Speciphy', __DIR__ . '/../src');
