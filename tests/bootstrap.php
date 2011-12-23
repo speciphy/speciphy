acf4741517a31cdec08f906cea51439bfae6f3e9
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-23 12:46:54 +0900

    Fix: Path to vendor.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/Speciphy/Tests/TestCase.php';
 require_once __DIR__ . '/../vendor/pear/PHPSpec/Loader/UniversalClassLoader.php';
 $loader = new \PHPSpec\Loader\UniversalClassLoader();
-$loader->registerNamespace('PHPSpec', __DIR__ . '/../vendor/php');
+$loader->registerNamespace('PHPSpec', __DIR__ . '/../vendor/pear');
 $loader->registerNamespace('Speciphy', __DIR__ . '/../src');
 $loader->register();
 
