7ff4165de023ab9b646f56a68ef1d95a8eec6011
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-25 15:16:16 +0900

    Change: Uses develop branch temporary.

--- install-phar.sh
+++ install-phar.sh
@@ -1,2 +1,2 @@
 wget https://github.com/downloads/Behat/Behat/behat.phar -O behat.phar
-wget https://github.com/c9s/Onion/raw/master/onion.phar -O onion.phar
+wget https://github.com/c9s/Onion/raw/develop/onion.phar -O onion.phar
