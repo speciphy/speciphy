d8cd5d4f46eb0d0453a11f99088412c8f97ea0b3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-07-01 18:03:42 +0900

    Add Composer.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__ . '/Speciphy/Tests/TestCase.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->add('Speciphy\Tests', __DIR__);
 require_once __DIR__ . '/../vendor/pear/PHPSpec/Loader/UniversalClassLoader.php';
 $loader = new \PHPSpec\Loader\UniversalClassLoader();
 $loader->registerNamespace('PHPSpec', __DIR__ . '/../vendor/pear');
-$loader->registerNamespace('Speciphy', __DIR__ . '/../src');
 $loader->register();
 
 new \Speciphy\DSL\Functions;
