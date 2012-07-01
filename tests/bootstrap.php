ea1ebd453c2dafad47686a1b1756d5c61222888a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-07-01 18:07:29 +0900

    Remove PHPSpec's Interceptor. (Tests passed but Featrures broken)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('Speciphy\Tests', __DIR__);
-require_once __DIR__ . '/../vendor/pear/PHPSpec/Loader/UniversalClassLoader.php';
-$loader = new \PHPSpec\Loader\UniversalClassLoader();
-$loader->registerNamespace('PHPSpec', __DIR__ . '/../vendor/pear');
-$loader->register();
 
 new \Speciphy\DSL\Functions;
